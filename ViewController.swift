ea3bf17538ffb63b094ae01f5c730bc4bbd29eff
ciecclm <clm@imnu.edu.cn>
2016-06-27 09:24:52 +0800

    首次commit
    啊啊啊啊啊

---
+++ ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  testxyz
+//
+//  Created by admin on 16/6/22.
+//  Copyright © 2016年 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var x: UITextField!
+    @IBOutlet weak var y: UITextField!
+    @IBOutlet weak var z: UITextField!
+    @IBAction func add(sender: AnyObject) {
+        var temp=0
+        temp=(Int)(x.text!)!+(Int)(y.text!)!
+        z.text="\(temp)"
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
